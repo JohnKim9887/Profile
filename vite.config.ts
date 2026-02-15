010a9624fd67302ea365be9c71a285559d75c013
Johnkim9887 <Johnkim9887@gmail.com>
2026-02-15 18:25:49 +1000

    change base directory for production

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/JohnKimProfile/' : '/',
+  base: mode === 'production' ? '/Profile/' : '/',
   build: {
     outDir: 'docs',
   },
